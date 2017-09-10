1d347ea99ce4d9ae50a860436ebe474a0b3a1809
Anwesh43 <anweshthecool0@gmail.com>
2017-09-11 00:03:27 +0530

    await must be inside async block

--- async_await_timeout_demo.js
+++ async_await_timeout_demo.js
@@ -1,13 +1,14 @@
 async function getTimeoutPromise(n,data) {
-    var promise = new Promise((resolve,reject)=>{
+    var promise = await new Promise((resolve,reject)=>{
         setTimeout(()=>{
             resolve({status:"success",...data})
         },n*1000)
     })
-    return promise
+    return  promise
 }
-const resp = await getTimeoutPromise(2,{text:"Using Async Await ,Hello World"})
-
-if(resp.status && resp.status == "success") {
-    document.getElementById('content').innerHTML = resp.text
-}
+const resp =  getTimeoutPromise(2,{text:"Using Async Await ,Hello World"})
+resp.then((data)=>{
+  if(data.status && data.status == "success") {
+      document.getElementById('content').innerHTML = data.text
+  }
+})
